00d6e70205de1a0b39b856d8d064a6f5bf76341f
Huggler <khuggler@uwyo.edu>
2019-02-21 16:09:21 -0700

    AdultSeasonalSurv

--- R/AdultSeasonalSurv.R
+++ R/AdultSeasonalSurv.R
@@ -1,5 +1,4 @@
 #' @title Conversion of adult capture data into the correct format for modeling seasonal (winter and summer) survival
-#
 #' @description Convert capture and cause-specific mortality data of adults into the proper format for conducting survival analysis.This function will convert raw data
 #' that contains start dates and mortality dates of individual animals and transform it into the proper format required by package survival
 #' to model seasonal survival.
@@ -13,15 +12,15 @@
 #' @param winterstart month in numeric format that is desired to start winter (e.g. 11)
 #' @param winterend month in numeric format that is desired for winter to end (e.g. 05)
 #' @param cause vector of causes that require censoring (e.g. collar_failure, capturemort, etc.)
-#' @param format format of startcol and mortcols
+#' @param dateformat format of startcol and mortcols
 #' @return Returns a data.frame with animal ID, Year of survival, start date of modeling, end date of modeling, time alive (months), and status of animal (alive/censored = 0, dead = 1)
 #' @keywords adult, seasonal, winter, summer, survival, kaplan-meier, analysis
 #' @export
 #' @examples
 #' \donttest{AdultSeasonal<-AdultSeasonalSurv(data = data, startcol = 'CaptureDate',uni = uni, UAIDcol = "UAID", mortcol = 'MortalityDate', yearstart = 2015, yearend = 2019, seasons = c('winter', 'summer'), winterstart = 11, winterend = 05, cause = c("CollarFailure", 'CaptureMort'))}
 
-AdultSeasonalSurv<-function(data, uni, UAIDcol, startcol, mortcol, yearstart, yearend, seasons = c('winter', 'summer'), winterstart, winterend, cause, format){
-  data[,mortcol]<-as.Date(data[,mortcol], format = format)
+AdultSeasonalSurv<-function(data, uni, UAIDcol, startcol, mortcol, yearstart, yearend, seasons = c('winter', 'summer'), winterstart, winterend, cause, dateformat){
+  data[,mortcol]<-as.Date(data[,mortcol], format = dateformat)
   Year<-yearstart:yearend
   season<-seasons
   c<-expand.grid(Year,seasons,KEEP.OUT.ATTRS = F)
@@ -33,20 +32,23 @@ AdultSeasonalSurv<-function(data, uni, UAIDcol, startcol, mortcol, yearstart, ye
   c$EndDate<-NA
   c$EndDate[1:(nrow(c)-1)]<-as.character(c$StartDate[2:nrow(c)])
   names(c)<-c('Year','Season','Month','StartDate','EndDate')
+  c<-c[2:nrow(c),]
 
   d<-data.frame()
   z<-data.frame()
 
   for(k in 1:length(uni)){
-    sub<-data[data[UAID] == uni[k],]
+    sub<-data[data[,UAIDcol] == uni[k],]
 
-    if(is.na(sub[,mortcol])){
-      date <-Sys.Date()
+    p<-nrow(sub)
+
+    if(is.na(sub[p,mortcol])){
+      date<-Sys.Date()
     }
-    if(!is.na(sub[,mortcol])){
-    date<-as.Date(max(sub[,mortcol], na.rm=T), format = format, origin = sub[,mortcol])
+    if(!is.na(sub[p,mortcol])){
+    date<-as.Date(max(sub[,mortcol], na.rm=T), format = dateformat, origin = sub[,mortcol])
     }
-    start<-as.Date(min(sub[,startcol], na.rm=T), format = format, origin = sub[,startcol])
+    start<-as.Date(min(sub[,startcol], na.rm=T), format = dateformat, origin = sub[,startcol])
     x<-nrow(c)
 
     for(l in 1:x){
@@ -55,11 +57,12 @@ AdultSeasonalSurv<-function(data, uni, UAIDcol, startcol, mortcol, yearstart, ye
       xxx$MortInd<-ifelse(date >= xxx[,4] & date <= xxx[,5],1, 0)
       SurvTime<-ifelse(xxx$MortInd == 1, as.character(difftime(date, xxx[,4], units = "days")/30.6), 6)
       SurvTime<-as.numeric(SurvTime)
-      Stat<-ifelse(m == 6 | date == Sys.Date(), 0, 1)
+      Stat<-ifelse(SurvTime == 6 | date == Sys.Date(), 0, 1)
       #Alive<-ifelse(date == Sys.Date(), 1, 0)
-      All<-data.frame(AID = sub[,UAID][1], Year = strftime(xxx[,4], format = "%Y"),
+      All<-data.frame(AID = sub[,UAIDcol][1], Year = strftime(xxx[,4], format = "%Y"),
                       StartDate = xxx[,4], EndDate = xxx[,5], Time= SurvTime,
-                      Status = ifelse(Stat == 0 & sub$X %in% cause, 0, Stat))
+                      Status = ifelse(Stat == 0 | sub$X[p] %in% cause, 0, Stat),
+                      SeasonYr = paste(xxx[,2], strftime(xxx[,4], format = "%Y"), sep = "_"))
 
       d<-rbind(d, All)
       d<-d[d$StartDate >= start,]

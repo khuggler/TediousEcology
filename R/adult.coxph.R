ed4af1c20a1070329fcdcd1154ac56423e874ac9
Huggler <khuggler@uwyo.edu>
2019-04-11 12:00:15 -0600

    adult.cox.mods

--- R/adult.coxph.R
+++ R/adult.coxph.R
@@ -1,10 +1,10 @@
 #' @title Stack and extract all spatial data to elk, coyotes, and mountain lions (RF Encounter)
 #' @description Stack rasters and extract points data for elk, coyotes, and mountain lions
-#' @param data to be used to build RF model
-#' @param uni number of cpus to parallelize on
-#' @param uaidcol percent of data (decimal format) to withold for training model (default 0.25)
-#' @param startcol number of trees (maximum) to attempt to split on
-#' @param mortcol path to raster stack
+#' @param data database with age column in it
+#' @param uni vector of unique ids in which survival should be calculated
+#' @param uaidcol column where aids live
+#' @param startcol name of column where capture date lives
+#' @param mortcol name of column where mortality date lives
 #' @param yearstart path to study area polygon (where you want to define availability)
 #' @param yearend path to where predicted RF map should be written
 #' @param seasons duh
@@ -13,16 +13,48 @@
 #' @param cause duh
 #' @param plot duh
 #' @param title duh
+#' @param grossweightcol name of column where gross weight is stored
+#' @param maxthickcol name of column where max thickness is stored
+#' @param bcscol name of column where bcs is stored
+#' @param seasoncol name of column where season (Fall/Spring) is located
 #' @return Returns a list object with RFData necessary to predict probably of use in RF models (elk, coyotes, and mountain lions)
 #' @keywords elk, coyote, mountain lion, random forest, extract, raster, sample
 #' @export
 
 
-adult.cox.mods<-function(data, uni, uaidcol,startcol,mortcol, yearstart, yearend, seasons, winterstart, winterend, cause, plot, title){
+adult.cox.mods<-function(data, uni, uaidcol,startcol,mortcol, yearstart, yearend, seasons, winterstart, winterend, cause, plot, title, grossweightcol, maxthickcol, bcscol, seasoncol){
 
   survdat<-TediousEcology::AdultSeasonalSurv(data = data, uni = uni, UAIDcol = UAIDcol, startcol = startcol, mortcol = mortcol, yearstart = yearstart,
                                              yearend = yearend, seasons = seasons, winterstart = winterstart, winterend = winterend,
                                              cause = cause, plot = plot, title = title)
 
+  surv.dat<-survdat[[2]]
+  surv.dat$aidyr<-paste(surv.dat$AID, surv.dat$SeasonYr, sep = "_")
 
+  data[,startcol]<-as.Date(data[,startcol], tryFormats = c('%m/%d/%Y', '%Y-%m-%d'))
+  data$month<-strftime(data[,startcol], format = "%m")
+  data$Season<-ifelse(data$month == "11" | data$month == "12", "winter", "summer")
+  data$Year<-strftime(data[,startcol], format = '%Y')
+  data$SeasonYr<-paste(data$Season, data$Year, sep = "_")
+  data$aidyr<-paste(data$UAID, data$SeasonYr, sep = "_")
+  ifbdat<-TediousEcology::IFBFat(x = data, weight = 'lbs', GrossweightColName = grossweightcol, maxthickName = maxthickcol, bcsName = bcscol, FetNum = 1.692, season = seasoncol)
+
+
+  ### bind ifbdat to survival data ###
+  uni<-unique(ifbdat$aidyr)
+
+  full.surv<-data.frame()
+  for(k in 1:length(uni)){
+    survsub<-surv.dat[surv.dat$aidyr == uni[k],]
+    if(nrow(survsub) == 0){next}
+    ifbsub<-ifbdat[ifbdat$aidyr == uni[k],]
+    ifbsub<-ifbsub[!duplicated(ifbsub$aidyr),]
+
+    survsub$ifbf<-ifbsub$scaledIFBFat
+    survsub$age<-ifbsub$new.age
+
+    full.surv<-rbind(survsub, full.surv)
+  }
+return(full.surv)
 }
+

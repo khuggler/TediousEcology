7f91976777d1d1e601400d3ec03f1b7a4636f083
Huggler <khuggler@uwyo.edu>
2019-12-05 11:01:41 -0700

    colhist

---
+++ R/colhist.R
@@ -0,0 +1,92 @@
+#' @title Conversion of adult capture data into capture history data
+#
+#' @description Convert capture data into a record of every serial number and start and end dates of every individual captured
+#' @param capdat data.frame with all the capture information
+#' @return Returns a data.frame with start and end date of every individual serial number along with an assigned animal ID)
+#' @keywords capture, animal ID, history
+#' @export
+#' @examples
+#' \donttest{Hist<-CollarHistory(capdat = yourdata, format = %m/%d/%Y)}
+
+colhist<-function(capdat){
+
+cap<-read.csv(capdat, stringsAsFactors = F)
+cap$CaptureDate<-as.Date(cap$CaptureDate, tryFormats = c("%m/%d/%Y", "%Y-%m-%d"))
+
+uni<-unique(cap$UAID)
+
+colhist<-data.frame(AID = NA, Ser1 = NA, Ser1Start = NA, Ser1End = NA,
+                  Ser2 = NA, Ser2Start = NA, Ser2End = NA,
+                    Ser3 = NA, Ser3Start = NA, Ser3End = NA,
+                    Ser4 = NA, Ser4Start = NA, Ser4End = NA,
+                   Ser5 = NA, Ser5Start = NA, Ser5End = NA)
+
+for(k in 1:length(uni)){
+  sub<-cap[cap$UAID == uni[k],]
+
+
+
+ sub$Serial<-ifelse(is.na(sub$Old.Serial.Number) & !is.na(sub$New.Serial.Number), sub$New.Serial.Number, ifelse(!is.na(sub$Old.Serial.Number) & !is.na(sub$New.Serial.Number), sub$New.Serial.Number,
+                                                                                                                sub$Old.Serial.Number))
+x<-nrow(sub)
+sub[x,]$MortalityDate<-ifelse(!is.na(sub[x,]$MortalityDate), sub[x,]$MortalityDate, as.character(Sys.Date()))
+unisers<-unique(sub$Serial)
+
+
+ colhist[k,]<-NA
+
+ for(i in 1:length(unisers)){
+
+
+   colsub<-sub[sub$Serial == unisers[i],]
+
+
+   if(i ==1){
+     colhist$Ser1[k]<-colsub$Serial[1]
+     colhist$Ser1Start[k]<-as.character(colsub$CaptureDate[1])
+     x<-nrow(colsub)
+     colhist$Ser1End[k]<-ifelse(!is.na(colsub$MortalityDate[x]), as.character(colsub$MortalityDate[x]), NA)
+   }
+
+   if(i == 2){
+     colhist$Ser2[k]<-colsub$Serial[1]
+     colhist$Ser2Start[k]<-as.character(colsub$CaptureDate[1])
+     colhist$Ser1End[k]<-as.character(colsub$CaptureDate[1]-1)
+     x<-nrow(colsub)
+     colhist$Ser2End[k]<-ifelse(!is.na(colsub$MortalityDate[x]), as.character(colsub$MortalityDate[x]), NA)
+
+   }
+
+   if(i == 3){
+     colhist$Ser3[k]<-colsub$Serial[1]
+     colhist$Ser3Start[k]<-as.character(colsub$CaptureDate[1])
+     colhist$Ser2End[k]<-as.character(colsub$CaptureDate[1]-1)
+     x<-nrow(colsub)
+     colhist$Ser3End[k]<-ifelse(!is.na(colsub$MortalityDate[x]), as.character(colsub$MortalityDate[x]), NA)
+
+   }
+
+   if(i == 4){
+     colhist$Ser4[k]<-colsub$Serial[1]
+     colhist$Ser4Start[k]<-as.character(colsub$CaptureDate[1])
+     colhist$Ser3End[k]<-as.character(colsub$CaptureDate[1]-1)
+     x<-nrow(colsub)
+     colhist$Ser4End[k]<-ifelse(!is.na(colsub$MortalityDate[x]), as.character(colsub$MortalityDate[x]), NA)
+
+   }
+
+   if(i == 5){
+     colhist$Ser5[k]<-colsub$Serial[1]
+     colhist$Ser5Start[k]<-as.character(colsub$CaptureDate[1])
+     colhist$Ser4End[k]<-as.character(colsub$CaptureDate[1]-1)
+     x<-nrow(colsub)
+     colhist$Ser5End[k]<-ifelse(!is.na(colsub$MortalityDate[x]), as.character(colsub$MortalityDate[x]), NA)
+
+   }
+
+   colhist$AID[k]<-colsub$UAID[1]
+
+
+ }
+}
+  }

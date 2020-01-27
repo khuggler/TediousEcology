0d3ff87f86fee049e37c73199ccc7d853b485c11
Huggler <khuggler@uwyo.edu>
2020-01-27 15:25:44 -0800

    append.age

--- R/append.age.R
+++ R/append.age.R
@@ -1,43 +1,55 @@
-#' @title append ages and calculate new age for every capture event
-#' @description append ages of animals to databaes and calculate age for every capture event
-#' @param agepath path to where age table lives
-#' @param cappath path to where capture data lives
-#' @return Returns original capture data.frame with age column of each animal
-#' @keywords age, capture
+#' @title append age to gps data 
+#' @description append ages of animals to gps data 
+#' @param cappath path to where capture data (with birth dates) lives
+#' @param idcol name of column where animal ID exists
+#' @param gps name of gps dataframe
+#' @param type type of append to do. Either "gps" or "database"
+#' @return Returns original gps data with an age column 
+#' @keywords age, capture, gps 
 #' @export
 
-append.age<-function(agepath, cappath){
-### append AID to ages ###
-age<-read.csv(agepath, stringsAsFactors = F)
-age<-age[,c(1:11)]
-age$uni<-paste(age$Species, age$Tooth.ID, age$Sex,sep = "_")
-uni<-unique(age$uni)
-age<-age[!duplicated(age),]
-
-cap<-read.csv(cappath, stringsAsFactors = F)
-cap$Species<-ifelse(cap$Species == "MMD", "MD", cap$Species)
-cap$Sex<-ifelse(cap$Sex == "f" | cap$Sex == "F", "F", "M")
-cap$uni<-paste(cap$Species, cap$Lab.Accession, cap$Sex, sep = "_")
-
-new.data<-data.frame()
-for(k in 1:length(uni)){
-  agesub<-age[age$uni == uni[k],]
-  agesub$Date<-as.Date(agesub$Date,tryFormats = c("%m/%d/%Y", '%Y-%m-%d'))
-  cap$CaptureDate<-as.Date(cap$CaptureDate,tryFormats = c("%m/%d/%Y", '%Y-%m-%d'))
-
-  aid<-cap[cap$uni == uni[k],]$UAID
-  capsub<-cap[cap$UAID %in% aid,]
-  capsub$age<-agesub$Age
-  startdate<-capsub[1,]$CaptureDate
-  startdate<-as.Date(startdate, tryFormats = c("%m/%d/%Y", '%Y-%m-%d'))
-
-  sub<-capsub
-  sub$diff<-abs(as.numeric(difftime(startdate, sub$CaptureDate, units = "days"))/365)
-  sub$new.age<-sub$age + sub$diff
-
-  new.data<-rbind(new.data, sub)
-  new.dat<-new.data[,c(1:58, 62)]
-
+append.age<-function(agepath, cappath, idcol, gps, type){
+  
+  
+  if(type == "gps"){
+  sub<-read.csv(cappath, stringsAsFactors = F)
+  sub$BirthDate<-as.Date(sub$BirthDate, format = "%m/%d/%Y")
+  sub.gps<-gps
+  sub.gps$Date<-strftime(sub.gps$TelemDate, format = "%Y-%m-%d")
+  
+  uni<-unique(sub[,idcol])
+  all.dat<-data.frame()
+  for(i in 1:length(uni)){
+    subsub<-sub[sub[,idcol] == uni[i],]
+    new.gps<-sub.gps[sub.gps$AID == uni[i],]
+    
+    new.gps$diff<-as.numeric(difftime(new.gps$Date, subsub$BirthDate[1], units = "days")/365)
+    new.gps$age<-round(new.gps$diff/0.5)*0.5
+    
+    all.dat<-rbind(new.gps, all.dat)
+    
+      }
+  
+  }
+  
+  if(type == "database"){
+    sub<-read.csv(cappath, stringsAsFactors = F)
+    sub$BirthDate<-as.Date(sub$BirthDate, format = "%m/%d/%Y")
+    sub$CaptureDate<-as.Date(sub$CaptureDate, format = "%m/%d/%Y")
+    uni<-unique(sub[,idcol])
+    
+    all.dat<-data.frame()
+    for(k in 1:length(uni[k],)){
+      subsub<-sub[sub$UAID == uni[k],]
+      subsub$diff<-as.numeric(difftime(subsub$CaptureDate, subsub$BirthDate[1], units = "days")/365)
+      subsub$Age<-round(subsub$diff/0.5)*0.5
+      
+      
+      all.dat<-rbind(all.dat, subsub)
+    }
+  }
+  
+  
+  return(all.dat)
 }
-return(new.dat)
-}
+

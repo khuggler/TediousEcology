32150abc5548da70dcaf0a70a2e4fa4e50c4ca1c
khuggler <khuggler@uwyo.edu>
2020-10-16 09:48:33 -0700

    clean.gps

--- R/clean.gps.R
+++ R/clean.gps.R
@@ -13,9 +13,9 @@
 clean.gps<-function(atsfold, vecpath, capdat, spp){
 
 #' =============================
-#' read in ATS data 
+#' read in ATS data
 #' =============================
-#' 
+#'
 
   if(spp == "cervid"){
 files<-list.files(atsfold, full.names = T)
@@ -23,9 +23,11 @@ files<-list.files(atsfold, full.names = T)
 atsdat<-data.frame()
 for(k in 1:length(files)){
   sub<-read.table(files[[k]], sep = ",", header = T)
-  
+
   atsdat<-rbind(sub, atsdat)
 }
+
+
 
 atsdat$Date<-as.Date(atsdat$Date, format = "%m/%d/%Y")
 atsdat$Hour<-sprintf("%02d", atsdat$Hour)
@@ -39,13 +41,13 @@ attributes(atsdat$TelemDate)$tzone<-'MST'
 #' ==========================================
 #' read in vectronics data and bind together
 #' =========================================
-#' 
+#'
 
 vec<-Part::getVec(vecpath)
 vec<-data.frame(vec)
 
 #' ======================================
-#' merge vectronics and ats data 
+#' merge vectronics and ats data
 #' ======================================
 vec<-vec[, c(1:7)]
 names(vec)[6:7]<-c('Latitude', 'Longitude')
@@ -93,14 +95,14 @@ for(i in 1:nrow(x)){
     if(l == 5){
       xxx<-xx[,c(1,14:16)]
     }
-    
+
     if(is.na(xxx[1,2])){next}
     ss<-gps[gps$CollarSerialNumber==xxx[1,2],]
     ss<-ss[(ss$Date> xxx[,3] + 7)&ss$Date< (xxx[,4] - 7),]
     ss<-ss[complete.cases(ss$Latitude),]
-    
+
     c<-cap[cap$UAID == xxx[1,1],]
-    
+
     #ss<-as.data.frame(ss)
     if(nrow(ss)==0){next}
     if(nrow(ss)>0){
@@ -109,14 +111,14 @@ for(i in 1:nrow(x)){
       ss$Spp<-c$Species[1]
       ss$FawnAID<-c$FawnAID[1]
     }
-    
+
     if(l == 1){
       ald<-ss
     }
     if(l>1){
       ald<-rbind(ald,ss)
     }
-    
+
   }
   if(i == 1){
     outsp<-ald
@@ -132,50 +134,50 @@ outsp2<-outsp2[complete.cases(outsp2$CollarSerialNumber),]
 
 return(outsp2)
   }
-  
+
   if(spp == "coyote"){
-    
+
 
       atsdat<-read.table("D:/Box Sync/DEER/Data/GPSData/YoteData/yotedata.txt", sep = ",", header = T)
-      
+
      atsdat$Date<-as.Date(atsdat$Date, format = "%m/%d/%Y")
     atsdat$Hour<-sprintf("%02d", atsdat$Hour)
     atsdat$Minute<-sprintf("%02d", atsdat$Minute)
     atsdat$time<-paste0(atsdat$Hour, ":", atsdat$Minute, ":", "00")
     atsdat$TelemDate<-paste(atsdat$Date, atsdat$time, sep = " ")
     atsdat$TelemDate<-as.POSIXct(atsdat$TelemDate, format = "%Y-%m-%d %H:%M:%S", tz = "GMT")
-    
+
     attributes(atsdat$TelemDate)$tzone<-'MST'
     gps<-atsdat
-    
-    
-    
+
+
+
     yote<-read.csv(capdat, stringsAsFactors = F)
     uni<-unique(yote$Serial)
-    
+
     #only pull gps data for animals that were ever captured#
     data<-subset(gps, CollarSerialNumber %in% uni)
-    
+
     # fix dates #
     yote$Date<-as.Date(yote$Date, format="%m/%d/%Y")
     yote$MortDate<-as.Date(yote$MortDate, format = "%m/%d/%Y")
     yote$MortDate<-ifelse(is.na(yote$MortDate), as.character(Sys.Date()), as.character(yote$MortDate))
     yote$MortDate<-as.Date(yote$MortDate, format = '%Y-%m-%d')
-    
+
     f<-data.frame()
     for(i in 1:length(uni)){
-      
+
       if(uni[i] == "39290"){next}
-      
+
       sub<-gps[gps$CollarSerialNumber == uni[i],]
       subsub<-yote[yote$Serial == uni[i],]
-      
+
       if(nrow(subsub) == 1){
         sub<-sub[sub$Date >= subsub$Date & sub$Date <= subsub$MortDate,]
         sub$AID<-subsub$AID
         sub$Sex<-subsub$Sex
       }
-      
+
       if(nrow(subsub)> 1){
         b<-nrow(subsub)
         for(k in 1:length(b)){
@@ -183,18 +185,18 @@ return(outsp2)
           sub$Sex<-ifelse(sub$Date >= subsub$Date[k]+1 & sub$Date <= subsub$MortDate[k], subsub$Sex[k], NA)
           sub$AID<-ifelse(sub$Date >= subsub$Date[k+1]+1 & sub$Date <= subsub$MortDate[k+1], subsub$AID[k+1], sub$AID)
           sub$Sex<-ifelse(sub$Date >= subsub$Date[k+1]+1 & sub$Date <= subsub$MortDate[k+1], subsub$Sex[k+1], sub$Sex)
-          
+
         }
       }
-      
+
       f<-rbind(sub, f)
       f<-f[complete.cases(f$AID),]
-      
-      
+
+
     }
-    
+
     return(f)
-    
+
   }
 
 }
